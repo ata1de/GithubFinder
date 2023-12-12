38e1bd08a7c4de4d7898f97e2be862402305f48e
unknown <teteulima@outlook.com>
2023-12-12 10:00:47 -0300

    added new function to search

--- github_finder/src/components/Search.tsx
+++ github_finder/src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState, KeyboardEvent} from 'react'
 
 type SearchProps = {
     loadUser: (userName:string) => Promise<void>;
@@ -10,16 +10,26 @@ import {BsSearch} from 'react-icons/bs'
 const Search = ({loadUser}:SearchProps) => {
     const [userName , setUserName] = useState('')
 
+    const handleKeyDown = (e:KeyboardEvent) =>{
+        if(e.key === 'Enter'){
+            loadUser(userName);
+        }
+    }
+
   return (
     <div className={classes.search}>
         <h2>Busque por um usuário</h2>
         <p>Conheça seus melhores repositórios</p>
         <div>
-            <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)}/>
+            <input 
+            type="text" 
+            placeholder='Digite o nome do usuário' 
+            onChange={(e) => setUserName(e.target.value)}
+            onKeyDown={handleKeyDown}
+            />
+            
             <button 
-            onClick={() => loadUser(userName)}
-            onKeyDown={() => loadUser(userName)}
-            >
+            onClick={() => loadUser(userName)}>
                 <BsSearch />
             </button>
         </div>
