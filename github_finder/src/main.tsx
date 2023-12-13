611de50fa41aba75cd9582f5a8040ac7c214b511
unknown <teteulima@outlook.com>
2023-12-13 15:11:50 -0300

    finish the main page

--- github_finder/src/main.tsx
+++ github_finder/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routers/Home.tsx'
+import Repos from './routers/Repos.tsx'
 
 const router  = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router  = createBrowserRouter([
       {
         path: '/',
         element: <Home />
+      },
+      {
+        path: '/repos/:username',
+        element: <Repos />
       }
     ]
 
--- github_finder/src/routers/Home.tsx
+++ github_finder/src/routers/Home.tsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
 import Search from '../components/Search'
 import { UserpProps } from '../types/user'
+import User from '../components/User'
+import Error from '../components/Error'
 
 const Home = () => {
     const [user, setUser] = useState<UserpProps | null>(null)
+    const [error, setError] = useState(false)
 
     const loadUser =async (userName:string) => {
+        setError(false)
+        setUser(null)
 
         const res = await fetch(`https://api.github.com/users/${userName}`)
 
         const data = await res.json()
+
+        if (res.status === 404){
+          setError(true);
+          return;
+        }
+
+        console.log(data)
 
         const {avatar_url, login, location, followers, following} = data
 
@@ -27,7 +39,9 @@ const Home = () => {
   return (
     <div>
         <Search loadUser={loadUser} />
-        {user && <p>{user.login}</p>}
+        {user && <User {...user} />}
+        {error && <Error />}
+        
     </div>
   )
 }
